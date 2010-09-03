03262638c1a859a9849938d005bacda3466864e6
bob <bob@macmini.local>
2010-09-03 12:10:55 -0700

    Hard coded connection string

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -5,14 +5,11 @@ db_config = YAML::load(File.read(RAILS_ROOT + "/config/database.yml"))
 if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
   mongo = db_config[Rails.env]
   
-  if mongo["uri"]
     MongoMapper.config = {RAILS_ENV => {'uri' => 'mongodb://psmisc:stayclassyyo@flame.mongohq.com:27082/wickedlist'}}
     MongoMapper.connect(RAILS_ENV)
-  else
-    MongoMapper.connection = Mongo::Connection.new(mongo['host'], mongo['port'], :logger => Rails.logger)
-    MongoMapper.database = mongo['database']
-    MongoMapper.database.authenticate(mongo['username'], mongo['password'])
-  end
+#    MongoMapper.connection = Mongo::Connection.new(mongo['host'], mongo['port'], :logger => Rails.logger)
+#    MongoMapper.database = mongo['database']
+#    MongoMapper.database.authenticate(mongo['username'], mongo['password'])
 end
 
 #identity map plugin for mongomapper
