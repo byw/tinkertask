744a08b01861d2019bb8890665fd8151ce0cec67
bob <bob@macmini.local>
2010-09-03 12:01:24 -0700

    Modified mongodb connection to use uri.

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -5,10 +5,14 @@ db_config = YAML::load(File.read(RAILS_ROOT + "/config/database.yml"))
 if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
   mongo = db_config[Rails.env]
   
-  MongoMapper.connection = Mongo::Connection.new(mongo['host'], mongo['port'], :logger => Rails.logger)
-  MongoMapper.database = mongo['database']
-  MongoMapper.database.authenticate(mongo['username'], mongo['password'])
-  
+  if mongo["uri"]
+    MongoMapper.config = {RAILS_ENV => {'uri' => mongo['uri']}}
+    MongoMapper.connect(RAILS_ENV)
+  else
+    MongoMapper.connection = Mongo::Connection.new(mongo['host'], mongo['port'], :logger => Rails.logger)
+    MongoMapper.database = mongo['database']
+    MongoMapper.database.authenticate(mongo['username'], mongo['password'])
+  end
 end
 
 #identity map plugin for mongomapper
