5c4abfc280aaa62c648a4a5916ffc6b604cf26a3
bob <bob@macmini.local>
2010-09-10 00:58:56 -0700

    Setup exceptional for exception monitoring.

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   config.gem "haml"
   config.gem "inherited_resources", :version => '1.0.6'
   config.gem "formtastic"
+  config.gem "exceptional"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
